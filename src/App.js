84b777f302f52d9c5373949104c826e71847c632
Mustafa <mhassanzadeh777@gmail.com>
2022-10-11 22:32:07 -0600

    added basic error checking for inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import siteLogo from './images/logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [invalidPeople, setInvalid] = useState();
+  const [invalidTip, isInvalid] = useState();
+  // const [isActive, setActive] = useState();
+
+  const handleChange = (e) => {
+    if(e.target.value === '0') {
+      setInvalid(true);
+    }else {
+      setInvalid(false);
+    }
+  }
+
+  const handleTip = (e) => {
+    if(isNaN(e.target.value) || e.target.value < 0) {
+      isInvalid(true);
+    }else {
+      isInvalid(false);
+    }
+  }
+
+  /*const toggleFocus = () => {
+    setActive(true);
+  }*/
+
   return (
     <div className="App">
       <img src={siteLogo} alt="splitter logo" className='centered'/>
       <div className="user-input-container">
           <div className="bill-input-container">
             <label htmlFor="bill">Bill</label>
-            <input type="text" name='billamount' id='billamount' />
+            <input
+              type="text"
+              name='billamount'
+              id='billamount'
+            />
           </div>
           <h3 className='tip-header'>Select Tip %</h3>
           <div className="tip-input-container">
@@ -20,12 +50,28 @@ function App() {
             <div className="right-column-container">
               <button className="tip-btn">10%</button>
               <button className="tip-btn">25%</button>
-              <button className="tip-btn btn-custom">Custom</button>
+              <input 
+                type="text"
+                placeholder='Custom'
+                id='btn-custom'
+                onChange={handleTip}
+               />
+               {invalidTip === true && <p className='invalid-tip-error'>Zero or higher only.</p>}
             </div>
           </div>
           <div className="people-input-container">
-            <label htmlFor="people">Number of People</label>
-            <input type="text" name='people' id='people-num' pattern='^[1-9]\d*$' />
+            <label htmlFor="people" className='num-people-label'>
+            Number of People
+            {invalidPeople === true && <p className='error-message'> Can't be zero</p>}
+            </label>
+            <input 
+              type="text" 
+              name='people' 
+              id='people-num'
+              className={invalidPeople ? "error-border" : ""}
+              pattern='^[1-9]\d*$'
+              onChange={handleChange}
+             />
           </div>
       </div>
     </div>
